76124102ca37267c8dc08b7a8c6877697c6d406a
maanrachid <maanhajrachid@hotmail.com>
2024-10-06 19:16:23 +0200

    Add project files.

---
+++ EmployeeAdmin/Controllers/EmployeesController.cs
@@ -0,0 +1,45 @@
+﻿using EmployeeAdmin.Data;
+using EmployeeAdmin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdmin.Controllers
+{
+    // localhost/api/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeesController(AppDbContext appdb1) {
+            this._appDbContext = appdb1;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            var allemp = _appDbContext.employees.ToList();
+            return Ok(allemp);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeDT e)
+        {
+            var employeeVar = new Employee()
+            {
+                Name = e.Name,
+                Email = e.Email,
+                Phone = e.Phone,
+                Salary = e.Salary
+            };
+
+            _appDbContext.employees.Add(employeeVar);
+            _appDbContext.SaveChanges();
+            return Ok(employeeVar);
+
+
+        }
+    }
+}
---
+++ EmployeeAdmin/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeAdmin.Models;
+namespace EmployeeAdmin.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> employees { get; set; }
+
+    }
+}
